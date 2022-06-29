dd62c7c855502e2fa6b5a2e93752886525e4aa70
ji-no <mfm.ellegarden@gmail.com>
2022-06-29 22:49:16 +0900

    直近の結果の表示を修正

--- components/RaceResultColumn.js
+++ components/RaceResultColumn.js
@@ -1,3 +1,4 @@
+import { Col, Container, Row } from 'react-bootstrap';
 import {convertColorByScore} from'../util/color';
 
 export default function RaceResultColumn(props) {
@@ -14,15 +15,19 @@ export default function RaceResultColumn(props) {
             break
     }
     return(
-        <td style={{backgroundColor: backgroundColor,width: "250px"}}>
-            <ul>
-                <li>{props.race.raceDate}</li>
-                <li>{props.race.raceName}</li>
-                <li>{props.race.stadium} {props.race.raceType}{props.race.raceLength} {props.race.raceCondition}</li>
-                <li>順位:{props.ranking}</li>
-                <li>フルタイム:<span style={convertColorByScore(props.fullTime)}>{props.fullTime > 0 ? props.fullTime.toFixed(1) : 0}</span></li>
-                <li>上がり:<span style={convertColorByScore(props.lastRapTime)}>{props.lastRapTime > 0 ? props.lastRapTime.toFixed(1) : 0}</span></li>
-            </ul>
+        <td style={{backgroundColor: backgroundColor,width: "270px"}}>
+            <Row>
+                <Col xs={8}>{props.race.raceName}</Col>
+                <Col xs={4}><span className="h3">{props.ranking}</span>着</Col>
+            </Row>
+            <Row style={{fontSize: "small"}}>
+                <Col xs={5}>{props.race.raceDate}</Col>
+                <Col xs={7}>{props.race.stadium} {props.race.raceType}{props.race.raceLength} {props.race.raceCondition}</Col>
+            </Row>
+            <Row>
+                <Col xs={5}><span style={{fontSize: "small"}}>上がり:</span><span style={convertColorByScore(props.lastRapTime)} className="h5">{props.lastRapTime > 0 ? props.lastRapTime.toFixed(1) : 0}</span></Col>
+                <Col xs={7}><span style={{fontSize: "small"}}>フルタイム:</span><span style={convertColorByScore(props.fullTime)} className="h5">{props.fullTime > 0 ? props.fullTime.toFixed(1) : 0}</span></Col>
+            </Row>
         </td>
     )
 }
