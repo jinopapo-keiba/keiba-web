1b29ce9d4c0531a91f4d7f83ad86d42e8c7a094b
ji-no <mfm.ellegarden@gmail.com>
2023-08-16 21:34:37 +0900

    重い処理があるのでローディング画面を追加

---
+++ components/Main.js
@@ -0,0 +1,91 @@
+import Link from "next/link";
+import React, { useEffect } from "react";
+import { Button, Card, Col, Container, Form, Nav, Navbar, Row, Spinner, Tab, Tabs } from "react-bootstrap";
+import { BeforeRaceMenu } from "./BeforeRaceMenu";
+import { RaceDetail } from "./RaceDetail";
+import RecentResult from "./RecentResult";
+import { useState } from 'react';
+import { useRouter } from "next/router";
+
+export default function Main({beforeRaces,race,stadiums,requestParam,recentResult}) {
+    const [loadingFlag, setLoadingFlag] = useState(false);
+    const router = useRouter()
+
+    useEffect(() => {
+        const handleStart = (url) => url !== router.asPath && setLoadingFlag(true)
+        const handleComplete = () => setLoadingFlag(false)
+    
+        router.events.on('routeChangeStart', handleStart)
+        router.events.on('routeChangeComplete', handleComplete)
+        router.events.on('routeChangeError', handleComplete)
+    
+        return () => {
+          router.events.off('routeChangeStart', handleStart)
+          router.events.off('routeChangeComplete', handleComplete)
+          router.events.off('routeChangeError', handleComplete)
+        }
+      })
+
+    const loadingComponent = (
+        <div className="d-flex align-items-center justify-content-center vh-100">
+            <Spinner animation="border" role="status" className="center-block"></Spinner>
+        </div>
+    );
+
+    const mainConmponent = (
+        <>
+        <BeforeRaceMenu beforeRaces={beforeRaces} race={race} />
+        <RaceDetail race={race} />
+        <Card className="m-3">
+            <Card.Header>
+                絞り込み条件
+            </Card.Header>
+            <Card.Body>
+                <Form action="/" method="get">
+                    <Form.Group className="mb-3">
+                        <h4>
+                            競技場
+                        </h4>
+                        {
+                            stadiums.map((stadium, index) => (
+                                <Form.Check inline label={stadium} id={`stadium${index}`} name="stadiums" value={stadium} defaultChecked={!!requestParam.stadiums && requestParam.stadiums.includes(stadium)} />
+                            ))
+                        }
+                    </Form.Group>
+                    <Row className="mb-3">
+                        <Form.Group as={Col} className="mb-3">
+                            <h4>
+                                最短距離
+                            </h4>
+                            <Form.Control type="number" name="minRaceLength" defaultValue={requestParam.minRaceLength} />
+                        </Form.Group>
+                        <Form.Group as={Col} className="mb-3">
+                            <h4>
+                                最長距離
+                            </h4>
+                            <Form.Control type="number" name="maxRaceLength" defaultValue={requestParam.maxRaceLength} />
+                        </Form.Group>
+                    </Row>
+                    <input name="raceId" defaultValue={race.id} hidden />
+                    <Button as="input" type="submit" variant="outline-dark" value="絞り込み" className="mr-3" />
+                    <Button href={`?raceId=${race.id}`} variant="outline-dark">リセット</Button>
+                </Form>
+            </Card.Body>
+        </Card>
+        <Tabs defaultActiveKey="recent">
+            <Tab eventKey="recent" title="直近レース">
+                <RecentResult {...recentResult} />
+            </Tab>
+        </Tabs>
+        </>
+    )
+
+    return (
+        <>
+            <Container style={{ maxWidth: "2000px" }}>
+                {loadingFlag && loadingComponent}
+                {!loadingFlag && mainConmponent}
+            </Container>
+        </>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import RecentResult from "../components/RecentResult"
 import RaceRepository from "../repository/RaceRepository"
 import BeforeRaceService from "../service/BeforeRaceService"
 import RecentResultService from "../service/RecentResultService"
+import Main from "../components/Main"
 
 export async function getServerSideProps(context) {
     const stadiumsParams = Array.isArray(context.query.stadiums) ? context.query.stadiums : [context.query.stadiums]
@@ -16,7 +17,7 @@ export async function getServerSideProps(context) {
     const stadiums = ["札幌","函館","新潟","福島","東京","中山","中京","京都","阪神","小倉"]
     return {
         props: {
-            recentReuslt: await recentReusltPromise,
+            recentResult: await recentReusltPromise,
             beforeRaces: await beforeRacesPromise,
             race: race[0],
             requestParam: context.query,
@@ -28,73 +29,7 @@ export async function getServerSideProps(context) {
 export default function Home(props) {
     return(
         <SSRProvider>
-            <main>
-                <Row>
-                    <Col md={2} className={"bg-dark"}>
-                        <Navbar variant="dark">
-                            <Nav className="flex-column">
-                                <Link href="/analytics" passHref>
-                                    <Nav.Link>
-                                        過去レース分析
-                                    </Nav.Link>
-                                </Link>
-                                <Link href="/" passHref>
-                                    <Nav.Link>
-                                        開催レース分析
-                                    </Nav.Link>
-                                </Link>
-                            </Nav>
-                        </Navbar>
-                    </Col>
-                    <Col md={10}>
-                        <Container style={{ maxWidth: "2000px" }}>
-                            <BeforeRaceMenu beforeRaces={props.beforeRaces} race={props.race} />
-                            <RaceDetail race={props.race} />
-                            <Card className="m-3">
-                                <Card.Header>
-                                    絞り込み条件
-                                </Card.Header>
-                                <Card.Body>
-                                    <Form action="/" method="get">
-                                        <Form.Group className="mb-3">
-                                            <h4>
-                                                競技場
-                                            </h4>
-                                            {
-                                                props.stadiums.map((stadium, index) => (
-                                                    <Form.Check inline label={stadium} id={`stadium${index}`} name="stadiums" value={stadium} defaultChecked={!!props.requestParam.stadiums && props.requestParam.stadiums.includes(stadium)}/>
-                                                ))
-                                            }
-                                        </Form.Group>
-                                        <Row className="mb-3">
-                                            <Form.Group as={Col} className="mb-3">
-                                                <h4>
-                                                    最短距離
-                                                </h4>
-                                                <Form.Control type="number" name="minRaceLength" defaultValue={props.requestParam.minRaceLength}/>
-                                            </Form.Group>
-                                            <Form.Group as={Col} className="mb-3">
-                                                <h4>
-                                                    最長距離
-                                                </h4>
-                                                <Form.Control type="number" name="maxRaceLength" defaultValue={props.requestParam.maxRaceLength}/>
-                                            </Form.Group>
-                                        </Row>
-                                        <input name="raceId" defaultValue={props.race.id} hidden/>
-                                        <Button as="input" type="submit" variant="outline-dark" value="絞り込み" className="mr-3"/>
-                                        <Button href={`?raceId=${props.race.id}`} variant="outline-dark">リセット</Button>
-                                    </Form>
-                                </Card.Body>
-                            </Card>
-                            <Tabs defaultActiveKey="recent">
-                                <Tab eventKey="recent" title="直近レース">
-                                    <RecentResult {...props.recentReuslt}/>
-                                </Tab>
-                            </Tabs>
-                        </Container>
-                    </Col>
-                </Row>
-            </main>
+            <Main recentResult={props.recentResult} beforeRaces={props.beforeRaces} race={props.race} requestParam={props.requestParam} stadiums={props.stadiums}></Main>
         </SSRProvider>
     )
 }
